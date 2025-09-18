bdd5109c9f0faba3274f95bae07d074cfdbec5f1
heyonglin <heyonglin@xmiles.com>
2025-09-18 19:07:59 +0800

    cd: 修改部署脚本相关

--- build-utils/paths.ts
+++ build-utils/paths.ts
@@ -1,4 +1,7 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-export const projectRootPath = path.resolve(__dirname, '..');
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export const projectRootPath = path.resolve(dirname, '..');
 export const corePackageRootPath = path.resolve(projectRootPath, 'packages/core');
